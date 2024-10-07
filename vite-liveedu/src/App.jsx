401a0a217e86e989bf39209ff10ca8c7957a0bbc
Miguel Angel <miguel.delta21@gmail.com>
2024-10-06 23:20:00 -0500

    Inicio React

---
+++ vite-liveedu/src/App.jsx
@@ -0,0 +1,12 @@
+import { WelcomeToReact } from './WelcomeToReact'
+
+function App() {
+  return (
+    <main className="flex flex-col h-screen items-center justify-center">
+        <WelcomeToReact />
+    </main>
+
+  )
+}
+
+export default App
---
+++ vite-liveedu/src/WelcomeToReact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function WelcomeToReact() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to the React App!
+        <br />
+        This is a simple text component styled using Tailwind CSS.
+      </h1>
+    </div>
+  );
+}
+
+export default WelcomeToReact;
