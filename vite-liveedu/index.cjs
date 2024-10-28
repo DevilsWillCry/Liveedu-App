63fcfbb4d934a9cce2f649f4ff4f86f57e6585fc
Miguel Angel <miguel.delta21@gmail.com>
2024-10-28 15:04:27 -0500

    Terminar Render

--- vite-liveedu/index.cjs
+++ vite-liveedu/index.cjs
@@ -1,9 +1,6 @@
-import jsonServer from 'json-server';
-import path from 'path';
-import express from 'express'; // Asegúrate de tener express instalado
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('./src/db/db.json');
+const router = jsonServer.router("./src/db/db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
